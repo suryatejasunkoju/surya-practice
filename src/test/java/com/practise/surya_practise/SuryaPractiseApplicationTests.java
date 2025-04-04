c6fc18a770cb148c03184e55ab3c118992e8838c
suryateja-sunkoju <suryateja@feelvaleo.com>
2025-04-04 09:55:18 +0530

    added initial code

---
+++ src/test/java/com/practise/surya_practise/SuryaPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.surya_practise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuryaPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
