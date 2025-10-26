df9529010255209cf30fd62e9c13b8cf95740f2f
suryateja sunkoju <138435841+suryateja-sunkoju@users.noreply.github.com>
2025-10-26 16:42:36 +0530

    RepeatAndMissingNumber all appraoches done

--- src/main/java/com/practise/surya_practise/DSA/StriversSdeSheet/day2/arrays_part_2/RepeatAndMissingNumber.java
+++ src/main/java/com/practise/surya_practise/DSA/StriversSdeSheet/day2/arrays_part_2/RepeatAndMissingNumber.java
@@ -114,10 +114,81 @@ public class RepeatAndMissingNumber implements DsaSolutionInterface {
         log.info("-->slow={}, fast={}", slow, fast);
         return Pair.of("O(2n)", "O(1)");
     }
+    public Pair<String> bestApproach1()
+    {
+        int[] arr = {6, 5, 7, 1, 8, 6, 4, 3, 2};
+        int length = arr.length;
+        int A=-1, B=-1; //A=2 times repeated no, B=missing no
+        int arrXor=0, oneToNXor=0;
+        for (int i = 0; i < length; i++) {
+            arrXor^=arr[i];
+            oneToNXor^=(i+1);
+        }
+        //A=3,B=4
+        int combinedXor = arrXor ^ oneToNXor; //=A^B
+        log.info("arrXor={}, oneToNXor={}, combinedXor={}", arrXor, oneToNXor, combinedXor);
+        //here, A & B are 2 different nos for sure.
+        //i.e,A^B=7 will have at-least 1 bit which is different in A & B. Let that differentiating bit is xth bit.
+        //when we are doing xor of arr no.s & 1 to n. [1^2^3^4^5  ^ 1^2^3^3^5]
+        //only repeating & missing nos appear odd no. of times, remaining all no.s appear even no. of times.
+        //all these no.s([1^2^3^4^5  ^ 1^2^3^3^5]) will fall into 0 th part or 1th part of xth bit.
+        //if we do xor of these parts, we will be left with repeating & missing no.s as these are the ones which repeat odd no. of times
+        //0th part:[2^4^5  ^ 2^5]=4
+        //1st part:[1^3^5  ^ 1^3^3^5]=3
+        //we got A & B but don't know which one is which, just iterate in arr & figure it out.
+        int differentiatingBitPos=0;
+        while (combinedXor%2==0)
+        {
+            differentiatingBitPos++;
+            combinedXor/=2;
+        }
+        log.info("differentiatingBitPos={}", differentiatingBitPos);
+        int zeroThPart=0, oneThPart=0, diffBitSetNo = (1 << differentiatingBitPos);
+        for (int i = 0; i < length; i++)
+        {
+            int arrNo=arr[i];
+            if((diffBitSetNo & arrNo) == 0)
+            {
+                zeroThPart^=arrNo;
+            }
+            else
+            {
+                oneThPart^=arrNo;
+            }
+            int naturalNo=(i+1);
+            if((diffBitSetNo & naturalNo) == 0)
+            {
+                zeroThPart^=naturalNo;
+            }
+            else {
+                oneThPart^=naturalNo;
+            }
+        }
+        log.info("zeroThPart={}, oneThPart={}", zeroThPart, oneThPart);
+
+        //assuming 0thPart is repeating
+        int freq=0;
+        for (int j : arr)
+        {
+            if (zeroThPart == j) {
+                freq++;
+            }
+        }
+        if(freq==2){
+            A=zeroThPart;
+            B=oneThPart;
+        }
+        else {
+            B=zeroThPart;
+            A=oneThPart;
+        }
+        log.info("bestApproach1, A={},B={}", A, B);
+        return Pair.of("O(3n)", "O(1)");
+    }
 
     @Override
     public Pair<String> bestApproach() {
-        int[] arr = {3,1,2,5,3};
+        int[] arr = {6, 5, 7, 1, 8, 6, 4, 3, 2};
         int length = arr.length;
         int A=-1, B=-1; //A=2 times repeated no, B=missing no
         int arrSum=0, arrSquareSum=0;
@@ -143,7 +214,7 @@ public class RepeatAndMissingNumber implements DsaSolutionInterface {
         //A+B=y/x
         //=========
         //2A=x+y/x
-        //A=(x^2+y)/2*x
+        //A=(x^2+y)/(2*x)
 
         //finding B:
         //A-B=x
@@ -151,11 +222,11 @@ public class RepeatAndMissingNumber implements DsaSolutionInterface {
 
         int sum1ToN = NumberUtils.consecutiveNosSum(1, length);
         int sumOfSquares1ToN = NumberUtils.consecutiveNosSquaresSum(1,length);
-        int intermediateResult = arrSquareSum - sum1ToN;
+        log.info("arrSum={}, arrSquareSum={}, sum1ToN={}, sumOfSquares1ToN={}", arrSum, arrSquareSum, sum1ToN, sumOfSquares1ToN);
         int x=arrSum-sum1ToN, y=arrSquareSum-sumOfSquares1ToN;
-        A=(x*x+y)/2*x;
+        A=(x*x+y)/(2*x);
         B=A-x;
-        log.info("bestApproach, A={},B={}", A,B);
-        return null;
+        log.info("bestApproach, x={}, y={}, A={}, B={}", x, y, A, B);
+        return Pair.of("O(n)", "O(1)");
     }
 }
